ccca6a716cb29e55d6fdf3dc5f5333fcd2b98391
Axel Lanuza <rojasf@ae.com>
2024-11-18 11:16:17 -0600

    26. Standalone components: en modulos tradicionales

--- 10-mapsApp/src/app/maps/maps.module.ts
+++ 10-mapsApp/src/app/maps/maps.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import mapboxgl from 'mapbox-gl';
+
 mapboxgl.accessToken = "pk.eyJ1Ijoia2lua3lzcXVhZCIsImEiOiJjbTNtMGx1NzIwYmtpMnZwbDRweHd2dTBjIn0._GovhjgLg9rhJy3B3LF2mQ"
 
 import {MiniMapComponent} from './components/mini-map/mini-map.component';
@@ -12,23 +13,27 @@ import {MarkersPageComponent} from './pages/markers-page/markers-page.component'
 import {PropertiesPageComponent} from './pages/properties-page/properties-page.component';
 import {ZoomRangePageComponent} from './pages/zoom-range-page/zoom-range-page.component';
 import {RouterOutlet} from "@angular/router";
-import { MapsRoutingModule} from "./maps-routing.module";
+import {MapsRoutingModule} from "./maps-routing.module";
+import {CounterAloneComponent} from "../alone/components/counter-alone/counter-alone.component";
+
+/* Standalo counter*/
 
 @NgModule({
-    declarations: [
-        MiniMapComponent,
-        SideMenuComponent,
-        MapsLayoutComponent,
-        FullScreenPageComponent,
-        MarkersPageComponent,
-        PropertiesPageComponent,
-        ZoomRangePageComponent
-    ],
-    imports: [
-        CommonModule,
-        RouterOutlet,
-        MapsRoutingModule
-    ]
+  declarations: [
+    MiniMapComponent,
+    SideMenuComponent,
+    MapsLayoutComponent,
+    FullScreenPageComponent,
+    MarkersPageComponent,
+    PropertiesPageComponent,
+    ZoomRangePageComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MapsRoutingModule,
+    CounterAloneComponent
+  ]
 })
 export class MapsModule {
 }
